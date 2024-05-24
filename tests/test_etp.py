339efd91e5da002934fab8d009d6e907abeb0c23
EzhikiPravyat <ezhiki.pravyat.mirom@gmail.com>
2024-05-24 11:51:31 +0300

    some changes

--- tests/test_etp.py
+++ tests/test_etp.py
@@ -1,18 +1,15 @@
 import requests as req
-import pytest
-from pytest import fixture
-from pytest import mark
+from pytest import fixture, mark
 import json
 
-def test_run(session):
+def test_run(session: req.Session):
     test_auth_etp(session)
     auth_token = test_token(session)
     link_token =  test_voting_link(auth_token, session)
     test_auth_ec(link_token, session)
 
-def test_auth_etp(s):
+def test_auth_etp(session: req.Session):
     url = 'https://demo2-gaz.etpgpb.ru/index.php?rpctype=direct&module=default&client=etp'
-
     payload = json.dumps({
         "action": "Authentication",
         "method": "login",
@@ -26,9 +23,9 @@ def test_auth_etp(s):
         "tid": 7,
         "token": "4LWnOKNHa9s06076FmuYxA"
     })
-    response = s.post(url=url, data=payload)
+    response = session.post(url, data=payload)
 
-def test_token(s):
+def test_token(session: req.Session):
     url = 'https://demo2-gaz.etpgpb.ru/index.php?rpctype=direct&module=default&client=etp'
     payload = json.dumps({
         "action": "Index",
@@ -38,12 +35,11 @@ def test_token(s):
         "tid": 13,
         "token": "0bCWL8aaWIc5gDsY4K5KIg"
     })
-    response = s.post(url=url, data=payload)
+    response = session.post(url, data=payload)
     return response.json()['result']['auth_token']
 
-def test_voting_link(auth_token, s):
+def test_voting_link(auth_token: str, session: req.Session):
     url = 'https://demo2-gaz.etpgpb.ru/index.php?rpctype=direct&module=default&client=etp'
-
     payload = json.dumps({
         "action": "User",
         "method": "getVotingLink",
@@ -54,13 +50,12 @@ def test_voting_link(auth_token, s):
         "tid": 7,
         "token": str(auth_token)
     })
-    response = s.post(url=url, data=payload)
+    response = session.post(url, data=payload)
     link_token = response.json()['result']['link'].split("=")[1]
     return link_token
 
-def test_auth_ec(link_token, s):
-    url = 'https://api-demo-vote.etpgpb.ru/v1/auth/get-auth-data/' + link_token
-
-    response = s.get(url=url)
+def test_auth_ec(link_token: str, session: req.Session):
+    url = f"https://api-demo-vote.etpgpb.ru/v1/auth/get-auth-data/{link_token}"
+    response = session.get(url)
     print(response.json()['user']['accessToken'])
     print(response.text)
--- tests/test_smoke_json.py
+++ tests/test_smoke_json.py
@@ -3,17 +3,15 @@ from pytest import mark
 from fixtures.config import read_config_json
 
 
-@mark.usefixtures('session') ##Перед запуском теста выполяется предусловие, в данном случае session
 def test_put_user_profile(put):
     config_data = read_config_json("/api/v2/user/profile")
     response = put(url=config_data['url'], json=config_data['body'])
 
-    assert response.status_code in (200 or 401)
+    assert response.status_code in (200, 401)
     validate(instance=response.json(), schema=config_data['valid_scheme'])
     
 @mark.usefixtures('session')
 def test_get_dictionaries_oksm(get):
     config_data = read_config_json("/api/v2/dictionaries/oksm")
     response = get(url=config_data['url'])
-    
-    assert response.status_code in (200 or 401)
+    assert response.status_code in (200, 401)
